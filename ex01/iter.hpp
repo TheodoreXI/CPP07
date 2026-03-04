61d4bb378bb7d57a718374c6906015ea078941c9
Abdelhadi Akroud <aakroud@e3r2p16.1337.ma>
2026-03-04 12:56:52 +0000

    maybe

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,20 +3,16 @@
 
 #include <cstddef>
 #include <iostream>
+#include <cmath>
 
 
-template <typename T>
-void iter(T *a, const size_t l, void (*f)(const T&))//must be changed to u
+template <typename T, typename U>
+void iter(T *a, const size_t l, U f)//must be changed to u
 {
 	for (size_t i = 0; i < l; i++)
 	{
 		f(a[i]);
 	}
-}
-template <typename T>
-void	f(const T &b)
-{
-	std::cout << b << "\n";
 }
 
 
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,8 +1,39 @@
 #include "iter.hpp"
 
+
+void inc(int &a)
+{
+	a++;
+}
+
+void	print(const std::string &s)
+{
+	std::cout << s << "\n";
+}
+
+void	ft_round(float &k)
+{
+	k = std::round(k);
+}
+
+
+
 int main()
 {
-	// int arr[] = {1,2,3,4,5};
-	std::string arr[] = {"hello", "lol"};
-	iter(arr, 2, f);
+	int arr[] = {1,2,3,4,5};
+	iter(arr, 5, inc);
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << "\n";
+	std::string s[] = {"hello", "lol"};
+	iter(s, 2, print);
+	float nums[] = {1.2, 4.7, 3.4, 5.6, 2.3};
+	iter(nums, 5, ft_round);
+	for (int i = 0; i < 5; i++)
+	{
+		std::cout << nums[i] << " ";
+	}
+	std::cout << "\n";
 }
